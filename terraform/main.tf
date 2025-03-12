c2bf5369ea51bf06b3b5db1ccb1ca8f022a3e422
Cell1911 <dalvisr191196@gmail.com>
2025-03-12 23:58:18 +0000

    third commit

---
+++ terraform/main.tf
@@ -0,0 +1,53 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.75.1"
+    }
+  }
+}
+
+provider "aws" {
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region     = var.aws_region
+}
+
+# Security Group
+resource "aws_security_group" "web_sg" {
+  name        = "web-server-sg"
+  description = "Allow SSH and HTTP"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0cb91c7de36eed2cb"
+  instance_type = var.instance_type
+  key_name      = var.key_name
+  security_groups = [aws_security_group.web_sg.name]
+  associate_public_ip_address = var.enable_public_ip
+
+  tags = {
+    Name = "Terraform-Web-Server"
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "aws_region" {
+  default = "us-east-2"
+}
+
+variable "key_name" {
+  default = "my-key"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "ami_id" {
+  default = "ami-0cb91c7de36eed2cb"  
+}
